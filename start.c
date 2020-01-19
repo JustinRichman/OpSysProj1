1f25a34027c6a8c07038e979d7ba8efbfa48290f
Justin Richman <jrichman2@ymail.com>
2020-01-19 12:13:28 -0500

    Made prompt, have understanding of env variables and the parsing(kinda)

---
+++ start.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <parser_help.c>
+
+int main() {
+
+  char* input;
+
+
+  //while(input != "logout" || input != "exit"){
+
+   printf("%s@%s : %s > \n", getenv("USER"), getenv("MACHINE"), getenv("PWD"));
+   scanf("%s", input);
+
+
+
+ //}
+
+
+}
